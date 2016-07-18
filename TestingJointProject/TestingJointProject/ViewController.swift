1efd7f6d32ffc64c35c09d227e66db4ab56b269e
jellybeanjohnny <mamerige@icloud.com>
2016-07-18 17:32:40 -0400

    fixed spacing

--- TestingJointProject/TestingJointProject/ViewController.swift
+++ TestingJointProject/TestingJointProject/ViewController.swift
@@ -9,19 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
 		print("hello")
-        print("this sht is bananas")
+		print("this sht is bananas")
+		
 	}
-
+	
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
 	}
-
-
+	
+	
 }
 
